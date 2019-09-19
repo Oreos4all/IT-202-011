2afd911a40420dc0bcbf3a89c903cfd855bcbb66
Oreos4all <jovannah1antoine@gmail.com>
2019-09-19 09:58:09 -0400

    testing

---
+++ initDB.php
@@ -0,0 +1,39 @@
+<?php
+#turn on error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require('config.php');
+echo "Loaded Host: " . $host;
+
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+
+try{
+	$db = new PDO($conn_string, $username, $password);
+	echo " Connected";
+	
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null, 
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
+	$insertTest = "INSERT INTO `joa23`.`TestUsers` (`id`, `username`, `pin`) VALUES ('1', 'username', '0');";
+    	$stmt = $db->prepare($insertTest);
+    	$r = $stmt->execute();
+		
+	 
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("Something went wrong");
+}
+
+
+?>
